57b6f57a198aa5defabf8e3573360f871d0598a4
AmKcit <amitkcc26@gmail.com>
2024-08-04 20:48:30 +0545

    now i've finished the ch1 of my new js begging

--- myfirst.js
+++ myfirst.js
@@ -26,12 +26,75 @@
 
 // const a = 10;
 // console.log(a);
-{
+// {
 
-    let a=5;
-    console.log(a);
-}
-{
-    let a=10;
-    console.log(a);
-}
+//     let a=5;
+//     console.log(a);
+// }
+// {
+//     let a=10;
+//     console.log(a);
+// }
+
+// let age = 24;
+// console.log(typeof(age));
+
+// let price = 100.5
+// console.log(typeof(price));
+
+// let fullName = "tonystark";
+// console.log(typeof(fullName));
+
+// let x = null;
+// console.log(typeof(x));
+
+
+// let y = BigInt(123);
+// console.log(typeof(y));
+
+// let z = Symbol('hello');
+// console.log(typeof(z));
+
+// const student = {
+//     name:'lekh maya',
+//     age: 20,
+//     cgpa:9.2,
+//     isPass:true,
+// };
+
+// student['age'] = student['age'] + 1;
+// student['name'] = "maya grg";
+
+
+
+// console.log(typeof(student));
+// console.log(student['name']);
+// console.log(student.age);
+// console.log(student.cgpa);
+
+
+const ballPen = {
+    productName:"ball_pen",
+    rating:4.5,
+    price: 100,
+    offer: 5,
+};
+// console.log(ballPen);
+// console.log(typeof(ballPen));
+
+const profile = {
+    userName:'sharada_khapara',
+    isFollow:true,
+    posts:195,
+    followers:56900,
+    following: 4,
+    bio: "apana college"
+};
+profile['userName']="maya grg";
+profile['posts'] = profile['posts'] + 1;
+profile['isFollow']=false;
+console.log(profile);  
+console.log(typeof(profile['isFollow']));
+
+
+// console.log(profile);
