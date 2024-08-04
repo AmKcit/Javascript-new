da6763c77a1d6d64325e66ff781b40d6d73c747e
AmKcit <amitkcc26@gmail.com>
2024-08-04 19:30:06 +0545

    again starting javascript coz my doubts are not clear

---
+++ myfirst.js
@@ -0,0 +1,37 @@
+// console.log('hello world');
+// console.log('hello world');
+
+// age = 24;
+// console.log(age);
+// console.log(age + 1);
+// fullName = "tony stark";
+// fullName = 25;
+// console.log(fullName);
+// price = 150.6
+// console.log(price);
+// age = 24;
+// console.log(age);
+// x = undefined;
+// console.log(x);
+// isFollow= false;
+// console.log(isFollow);
+
+// let fullName = "tony stark";
+// let age = 24;
+// let totalPrice = 1000;
+
+// console.log(fullName);
+// console.log(age);
+// console.log(totalPrice);
+
+// const a = 10;
+// console.log(a);
+{
+
+    let a=5;
+    console.log(a);
+}
+{
+    let a=10;
+    console.log(a);
+}
