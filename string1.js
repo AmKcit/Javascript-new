9ad764f1e30ca05f71f3e4b3eefffedbbd894703
AmKcit <amitkcc26@gmail.com>
2024-08-06 20:19:26 +0545

    this is the 4th chapter of js

--- string1.js
+++ string1.js
@@ -1,5 +1,15 @@
 // strings
 
-let str = 'amit';
-let str2 = "kc";
-console.log(str.length);
+// let str = 'amit';
+// let str2 = "kc";
+// console.log(str[2]);
+
+// Template literals
+
+let obj = {
+    item: 'pen',
+    price: 10,
+};
+console.log('this is the cost',obj.item,'is',obj.price,'rupees' );
+let sentence = `this is a template iterals`;
+console.log(typeof(sentence));
