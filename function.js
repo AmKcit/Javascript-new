a3915cdf144c928069a5ff8c886f671d32533f18
AmKcit <amitkcc26@gmail.com>
2024-08-28 18:27:49 +0545

    oopo gandu

--- function.js
+++ function.js
@@ -40,7 +40,17 @@
 
 //practice question
 
-function countVowels(str){
+// function countVowels(str){
+//     let count=0;
+//     for(const char of str){
+//         if(char ==='a'||char ==="e" || char ==="i" || char ==="o" || char ==="u"){
+//             count++;  
+//         }
+//     }
+//     return count;
+// }
+
+const countVowels = (str) => {
     let count=0;
     for(const char of str){
         if(char ==='a'||char ==="e" || char ==="i" || char ==="o" || char ==="u"){
@@ -49,3 +59,4 @@ function countVowels(str){
     }
     return count;
 }
+console.log(countVowels("aeiou"));
